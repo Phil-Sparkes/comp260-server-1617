9b5296a15d721b4059c67db839900481a8c7c768
ps196806 <phil.sparkes95@gmail.com>
2018-02-21 10:47:12 +0000

    project

---
+++ MUD2/Server/Server/Dungeon.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    public class Dungeon
+    {
+        Dictionary<String, Room> roomMap;
+
+        Room currentRoom;
+
+        public void Init()
+        {
+            roomMap = new Dictionary<string, Room>();
+            {
+                var room = new Room("Room 0", "You are standing in the entrance hall\nAll adventures start here");
+                room.north = "Room 1";
+                roomMap.Add(room.name, room);
+            }
+
+            {
+                var room = new Room("Room 1", "You are in room 1");
+                room.south = "Room 0";
+                room.west = "Room 3";
+                room.east = "Room 2";
+                roomMap.Add(room.name, room);
+            }
+
+            {
+                var room = new Room("Room 2", "You are in room 2");
+                room.north = "Room 4";
+                roomMap.Add(room.name, room);
+            }
+
+            {
+                var room = new Room("Room 3", "You are in room 3");
+                room.east = "Room 1";
+                roomMap.Add(room.name, room);
+            }
+
+            {
+                var room = new Room("Room 4", "You are in room 4");
+                room.south = "Room 2";
+                room.west = "Room 5";
+                roomMap.Add(room.name, room);
+            }
+
+            {
+                var room = new Room("Room 5", "You are in room 5");
+                room.south = "Room 1";
+                room.east = "Room 4";
+                roomMap.Add(room.name, room);
+            }
+
+            currentRoom = roomMap["Room 0"];
+        }
+
+        public String Options()
+        {
+            String MsgToSend = "";
+            MsgToSend += currentRoom.desc;
+            MsgToSend += "\nExits\n";
+            for (var i = 0; i < currentRoom.exits.Length; i++)
+            {
+                if (currentRoom.exits[i] != null)
+                {
+                    MsgToSend += (Room.exitNames[i] + " ");
+                }
+            }
+            return MsgToSend;
+
+        }
+        public String Process(string message)
+        {
+            //var key = Console.ReadLine();
+            var key = message;
+            String MsgToSend = "";
+            var input = key.Split(' ');
+
+            switch (input[0].ToLower())
+            {
+                case "help":
+                    Console.Clear();
+                    MsgToSend += "\nCommands are ..../n";
+                    MsgToSend += "help - for this screen/n";
+                    MsgToSend += "look - to look around/n";
+                    MsgToSend += "go [north | south | east | west]  - to travel between locations/n";
+                    MsgToSend += "\nPress any key to continue/n";
+
+                    return MsgToSend;
+
+                case "look":
+                    //loop straight back
+                    Console.Clear();
+                    Thread.Sleep(1000);
+                    return MsgToSend;
+
+                case "say":
+                    Console.Write("You say ");
+                    for (var i = 1; i < input.Length; i++)
+                    {
+                        Console.Write(input[i] + " ");
+                    }
+
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    return MsgToSend;
+
+                case "go":
+                    // is arg[1] sensible?
+                    if ((input[1].ToLower() == "north") && (currentRoom.north != null))
+                    {
+                        currentRoom = roomMap[currentRoom.north];
+                    }
+                    else
+                    {
+                        if ((input[1].ToLower() == "south") && (currentRoom.south != null))
+                        {
+                            currentRoom = roomMap[currentRoom.south];
+                        }
+                        else
+                        {
+                            if ((input[1].ToLower() == "east") && (currentRoom.east != null))
+                            {
+                                currentRoom = roomMap[currentRoom.east];
+                            }
+                            else
+                            {
+                                if ((input[1].ToLower() == "west") && (currentRoom.west != null))
+                                {
+                                    currentRoom = roomMap[currentRoom.west];
+                                }
+                                else
+                                {
+                                    //handle error
+                                    MsgToSend += "\nERROR";
+                                    MsgToSend += "\nCan not go " + input[1] + " from here";
+                                    MsgToSend += "\nPress any key to continue";
+                                }
+                            }
+                        }
+                    }
+                    return MsgToSend;
+
+                default:
+                    //handle error
+                    MsgToSend += "\nERROR";
+                    MsgToSend += "\nCan not " + key;
+                    MsgToSend += "\nPress any key to continue\n";
+                    return MsgToSend;
+            }
+
+        }
+    }
+}
---
+++ MUD2/Server/Server/server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class server
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ipLocal = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8221);
+			
+            s.Bind(ipLocal);
+            s.Listen(4);
+
+            Console.WriteLine("Waiting for client ...");
+
+            var dungeon = new Dungeon(); // make the dungeon
+            dungeon.Init();
+
+            Console.WriteLine(dungeon.Options());
+
+            Socket newConnection = s.Accept();
+            if (newConnection != null)
+            {            
+                while (true)
+                {
+                    byte[] buffer = new byte[4096];
+
+                    try
+                    {
+                        int result = newConnection.Receive(buffer);
+
+                        if (result > 0)
+                        {
+                            ASCIIEncoding encoder = new ASCIIEncoding();
+                            String recdMsg = encoder.GetString(buffer, 0, result);
+
+                            Console.WriteLine("Received: " + recdMsg);
+
+                            Console.WriteLine(dungeon.Process(recdMsg));
+                            Console.Clear();
+                            Console.WriteLine(dungeon.Options());
+
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex);    	
+                    }                    
+                }
+            }
+        }
+    }
+}
